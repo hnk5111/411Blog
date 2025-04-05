376ab8cc1419b3ad4802744c723a21cc47651ddc
SkylerKoba88 <sfk5879@psu.edu>
2025-04-05 03:11:05 -0400

    added partial stiff

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,17 @@ const router = express.Router();
 
 // Routes
 router.get('', (req, res) => {
-    res.send("Hello World");
+    const locals = {
+        title: "NodeJS Blog",
+        description: "Simple blog created using NodeJS, Express & MongoDB."
+    }
+
+
+    res.render('index', {locals});
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
 });
 
 module.exports = router;
