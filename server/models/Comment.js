a8ef8525b44f7c2880f2cbcc9ef0515a27738eec
SkylerKoba88 <sfk5879@psu.edu>
2025-04-18 14:05:52 -0400

    trying to make comments

---
+++ server/models/Comment.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const CommentSchema = new Schema ({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Comment', CommentSchema);
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
+const Comment = require('../models/Comment');
 
 /*
  * GET /
@@ -104,6 +105,117 @@ router.post('/search', async (req, res) => {
     } catch (error) {
         console.log(error);
     }
+});
+
+/**
+ * GET /
+ * Comment :id
+ */
+router.get('post/comment/:id', async (req, res) => {
+    try {
+        let slug = req.params.id;
+    
+        const data = await Comment.findById({ _id: slug });
+
+        const locals = {
+            title: data.title,
+            description: "Simple Blog created with NodeJs, Express, & MongoDB.",
+        }
+
+        res.render('comment', { locals, data, currentRoute: `/post/comment/${slug}`, });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+/**
+ * GET /
+ * Create New Comment
+*/
+router.get('/add-comment', authMiddleware, async (req, res) => {
+  try {
+    const locals = {
+      title: 'Add Comment',
+      description: 'Simple Blog created with NodeJs, Express & MongoDb.'
+    }
+
+    const data = await Comment.find();
+    res.render('post/add-comment', {
+      locals,
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+
+/**
+ * POST /
+ * Create New Comment
+*/
+router.post('/add-comment', authMiddleware, async (req, res) => {
+  try {
+    try{
+      const newComment = new Comment({
+        title: req.body.title,
+        body: req.body.body
+      });
+
+      await Comment.create(newComment);
+      res.redirect('/post/');
+    }catch(error){
+      console.log(error);
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+
+/**
+ * GET /
+ * Create New Comment
+*/
+router.get('/edit-comment/:id', authMiddleware, async (req, res) => {
+  try {
+    const locals = {
+      title: 'Edit Comment',
+      description: 'Free NodeJs User Management System.',
+    }
+
+    const data = await Comment.findOne({ _id: req.params.id });
+    res.render('post/edit-comment', {
+      locals,
+      data,
+    })
+
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+/**
+ * PUT /
+ * Create New Comment
+*/
+router.put('/edit-comment/:id', authMiddleware, async (req, res) => {
+  try {
+    await Comment.findByIdAndUpdate(req.params.id, {
+      title: req.body.title,
+      body: req.body.body,
+      updatedAt: Date.now(),
+    });
+    res.redirect(`/edit-comment/${req.params.id}`)
+
+  } catch (error) {
+    console.log(error);
+  }
+
 });
 
 
